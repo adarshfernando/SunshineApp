c4aaf00cd203011d2a8f3edf9f1b8af400c63e54
adarshfernando <adarsh.fernando@gmail.com>
2016-01-04 12:30:53 -0800

    Added Temperature Units Setting - Allow user to choose from Metric or Imperial Units - Updated string.xml with string values for temperature setting - Created array.xml to link unit labels to their values - Updated pref_general.xml with ListPreference for setting - Updated SettingsActivity.java to include new bindPreferenceSummaryToValue() - Updated formatHighLows in ForecastFragment.java to accept String param for user unit preference. String is evaluated and temperatures are converted to Imperial if required; metric by default.

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -148,7 +148,15 @@ public class ForecastFragment extends Fragment {
         /**
          * Prepare the weather high/lows for presentation.
          */
-        private String formatHighLows(double high, double low) {
+        private String formatHighLows(double high, double low, String unitType) {
+
+            if (unitType.equals(getString(R.string.pref_units_imperial))) {
+                high = (high * 1.8) +32;
+                low = (low * 1.8) +32;
+            } else if (!unitType.equals(getString(R.string.pref_units_metric))) {
+                Log.d(LOG_TAG, "Unit type not found: " + unitType);
+            }
+
             // For presentation, assume the user doesn't care about tenths of a degree.
             long roundedHigh = Math.round(high);
             long roundedLow = Math.round(low);
@@ -195,6 +203,12 @@ public class ForecastFragment extends Fragment {
             dayTime = new Time();
 
             String[] resultStrs = new String[numDays];
+
+            SharedPreferences sharedPrefs =
+                    PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String unitType = sharedPrefs.getString(
+                    getString(R.string.pref_units_key), getString(R.string.pref_units_default));
+
             for (int i = 0; i < weatherArray.length(); i++) {
                 // For now, using the format "Day, description, hi/low"
                 String day;
@@ -218,11 +232,12 @@ public class ForecastFragment extends Fragment {
 
                 // Temperatures are in a child object called "temp".  Try not to name variables
                 // "temp" when working with temperature.  It confuses everybody.
+
                 JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
                 double high = temperatureObject.getDouble(OWM_MAX);
                 double low = temperatureObject.getDouble(OWM_MIN);
 
-                highAndLow = formatHighLows(high, low);
+                highAndLow = formatHighLows(high, low, unitType);
                 resultStrs[i] = day + ": " + description + ", " + highAndLow;
             }
 
